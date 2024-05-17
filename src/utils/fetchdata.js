6d13346d7c3c65421fa9d90f4120f412f07f1f27
Jacob <jacob@joinatlas.xyz>
2024-05-17 14:56:08 -0400

    update responsive styles

--- src/utils/fetchdata.js
+++ src/utils/fetchdata.js
@@ -1,14 +1,15 @@
 import { sendRequestToOpenAI } from "../api.js";
 
-export async function fetchData() {
+export async function fetchData(fid) {
   const NEYNAR_API_KEY = import.meta.env.NEYNAR_API_KEY;
-  const URL = "https://api.neynar.com/v1/farcaster/casts?fid=3&limit=6";
+  const URL = `https://api.neynar.com/v1/farcaster/casts?fid=${fid}&limit=6`;
   const headersX = {
     accept: "application/json",
     api_key: NEYNAR_API_KEY,
   };
 
   try {
+    console.log(`Fetching data with fid: ${fid}`); // Debugging line to check fid
     const responseNeynar = await fetch(URL, { headers: headersX });
     const dataNeynar = await responseNeynar.json();
     const casts = dataNeynar.result.casts;
@@ -16,7 +17,7 @@ export async function fetchData() {
     const allCastsText = casts.map((cast) => cast.text).join(" ");
 
     const openAIResponse = await sendRequestToOpenAI(
-      `You are Vector, the wolf guide, known for your insightful yet concise greetings. With just a few words, you reflect the essence of new arrivals in the Fractaline Forest. Drawing from the player's latest thoughts, craft a brief welcome that hints at their inner world, starting with 'Welcome, traveler'. Keep your message under 15 words, and make reference to at least one of the messages in: ${allCastsText}.`
+      `You are Vector, the wolf guide, who knows the soul of each traveler. Craft a brief welcoming question that hints at the inner world of the new arrival, beginning with 'Welcome, traveler'. Keep your message under 18 words, and refer to at least one of the thoughts in: ${allCastsText}.`
     );
 
     let firstChoiceText = "";
